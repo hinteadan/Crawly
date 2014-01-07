2f07d38f255e93b68691d4919f593d7638afa5f2
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-01-07 15:17:57 +0200

    Empty solution

---
+++ Source/CrawlySuite/CrawlyHttpHandler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrawlyHttpHandler")]
+[assembly: AssemblyDescription("ASP.NET HTTP Handler for generating indexable content of AJAX pages")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hintea Dan Alexandru")]
+[assembly: AssemblyProduct("CrawlyHttpHandler")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9690caa5-e4b1-4a08-803d-8c71d1c7c66e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
