b97ff32012fda9d1ab7a9962b786c3203e31af6e
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-01-07 17:47:51 +0200

    Define Crawly Template

--- Source/CrawlySuite/CrawlyConsolePlayground/Program.cs
+++ Source/CrawlySuite/CrawlyConsolePlayground/Program.cs
@@ -1,11 +1,18 @@
-﻿
-namespace H.Crawly.Console
+﻿using System;
+
+namespace H.Crawly
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string rawTemplate = "My name is @test and @test asdjkfaskldjf";
 
+            string test = new Crawly.Content.TestContentTemplate(rawTemplate).Result();
+
+            Console.WriteLine("Done @ {0}", DateTime.Now);
+            Console.WriteLine("Press key to exit.");
+            Console.ReadLine();
         }
     }
 }
---
+++ Source/CrawlySuite/CrawlyContentProcessor/CrawlyContentTemplate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H.Crawly.Content
+{
+    public abstract class CrawlyContentTemplate
+    {
+        private const char parameterMark = '@';
+        private static readonly char[] parameterEndMarks = new char[] { ' ', '.', '!', '?', '&', '<', '>', '|', '/', '\\', ':', ';', '"', '\'' };
+        private readonly Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        protected virtual string RawTemplateContent { get; set; }
+        protected Dictionary<string, string> Parameters { get { return parameters; } }
+
+        public string Result()
+        {
+            string templateContent = RawTemplateContent;
+            string resultContent = string.Empty;
+
+            if (string.IsNullOrEmpty(templateContent))
+            {
+                throw new InvalidOperationException("The RawTemplateContent must be set before processing the result");
+            }
+
+            int paramPosition = 0;
+            string paramName;
+            string value;
+            do
+            {
+                paramPosition = templateContent.IndexOf(parameterMark);
+                if (paramPosition > 0 && paramPosition < templateContent.Length - 1 && templateContent[paramPosition + 1] == parameterMark)
+                {
+                    paramPosition += 2;
+                    continue;
+                }
+                if (paramPosition < 0)
+                {
+                    resultContent += templateContent;
+                    break;
+                }
+                int paramEndPosition = templateContent.IndexOfAny(parameterEndMarks, paramPosition + 1);
+                if (paramEndPosition < 0)
+                {
+                    paramEndPosition = templateContent.Length - 1;
+                }
+                if (paramPosition + 1 == paramEndPosition)
+                {
+                    throw new System.FormatException("Template parameters MUST have names. If you want to use the parameter delimiter as content, escape it by doubling it.");
+                }
+                paramName = templateContent.Substring(paramPosition + 1, paramEndPosition - paramPosition - 1);
+                value = parameters.ContainsKey(paramName) ? parameters[paramName] : string.Empty;
+                resultContent += string.Format("{0}{1}{2}", templateContent.Substring(0, paramPosition), value, templateContent[paramEndPosition]);
+                templateContent = templateContent.Substring(paramEndPosition + 1);
+            } while (paramPosition >= 0);
+
+            return resultContent;
+        }
+    }
+
+    public class TestContentTemplate : CrawlyContentTemplate
+    {
+        public TestContentTemplate(string content)
+        {
+            base.RawTemplateContent = content;
+            base.Parameters.Add("test", "THIS_IS_THE_REPLACEMENT");
+        }
+    }
+}
