c814394c485d56c242c6c5f8342b6f2d62ea6908
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-01-11 19:22:04 +0200

    Scaffold option for generic HTTP Handler as alternative to extending Crawly HTTP Handler

---
+++ Source/CrawlySuite/CrawlyHttpHandler/GenericCrawlyHttpHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using H.Crawly.Common;
+using H.Crawly.Content;
+
+namespace H.Crawly
+{
+    public class GenericCrawlyHttpHandler<T> : CrawlyHttpHandler where T : IPopulateCrawlyTemplate
+    {
+
+    }
+}
---
+++ Source/CrawlySuite/WebPlayground/CustomTemplateDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using H.Crawly.Common;
+
+namespace H.Crawly.Web.Playground
+{
+    public class CustomTemplateDataSource : IPopulateCrawlyTemplate
+    {
+        public Dictionary<string, string> With()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
